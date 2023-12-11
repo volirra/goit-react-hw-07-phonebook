2a13857302f6d1befe68c4e596e2af500aa4297b
volirra <127318805+volirra@users.noreply.github.com>
2023-12-11 13:37:18 +0200

    commit

---
+++ src/service/API.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const contactsAPI = axios.create({
+  baseURL: '',
+});
+
+export const getContacts = async () => {
+  const { data } = await contactsAPI.get();
+  return data;
+};
+
+export const deleteContact = async id => {
+  const { data } = await contactsAPI.delete(id);
+  return data;
+};
+
+export const addContact = async contact => {
+  const { data } = await contactsAPI.post('', contact);
+  return data;
+};
